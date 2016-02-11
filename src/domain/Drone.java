53a0d8b6ec290adde660e503d42e36a6682b9f18
Bart <Bart@193.190.83.192>
2016-02-11 20:25:40 +0100

    MoveDrone will now set coordinate in Drone after a successful move

--- src/domain/Drone.java
+++ src/domain/Drone.java
@@ -1,11 +1,13 @@
 package domain;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Drone {
     private Coordinate currentLocation;
     private int maxUnits;
     private List<Product> products;
+    private int currentUnits;
 
     public Drone(Coordinate coords, int maxUnits, List<Product> products) {
         this.currentLocation = coords;
@@ -37,5 +39,59 @@ public class Drone {
         this.products = products;
     }  
     
+    public boolean loadProduct(Product product)
+    {
+        if(currentUnits + product.getUnits() <= maxUnits)
+        {
+            products.add(product);
+            currentUnits += product.getUnits();
+            return true;
+        }
+        return false;
+    }
     
+    public boolean unloadProduct(Product product)
+    {
+        Iterator<Product> it = products.iterator();
+        Product p;
+        boolean result = false;
+        while(it.hasNext())
+        {
+            p = it.next();
+            if(p.getType() == product.getType())
+            {
+                it.remove();
+                currentUnits -= p.getUnits();
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+    
+    public int unloadProduct(Product product, int amount)
+    {
+        int i = 0;
+        for(;i < amount; i++)
+        {
+            if(!unloadProduct(product))
+            {
+                break;
+            }
+        }
+        return i;
+    }
+    
+    public int loadProduct(Product product, int amount)
+    {
+        int i = 0;
+        for(;i < amount; i++)
+        {
+            if(!loadProduct(product))
+            {
+                break;
+            }
+        }
+        return i;
+    }
 }
