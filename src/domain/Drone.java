cff847631c3348ef22a47f766182a5efdcfbcff2
Axl Francois <axlfrancois@gmail.com>
2016-02-11 19:18:31 +0100

    Added objects

--- src/domain/Drone.java
+++ src/domain/Drone.java
@@ -1,5 +1,6 @@
 package domain;
 
 public class Drone {
+    private Coordinate coords;
     
 }
---
+++ src/domain/Product.java
@@ -0,0 +1,29 @@
+package domain;
+
+public class Product {
+    private int type;
+    private int units;
+
+    public Product(int type, int units) {
+        this.type = type;
+        this.units = units;
+    }
+    
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public void setUnits(int units) {
+        this.units = units;
+    }
+    
+    
+}
